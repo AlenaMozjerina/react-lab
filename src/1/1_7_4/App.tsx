9cff58e8447cab3061dfca9b340401f252683b04
AlenaMozjerina <alenamozjerina10@gmail.com>
2025-05-12 02:42:41 +0500

    1_7_4

--- src/1/1_7_4/App.tsx
+++ src/1/1_7_4/App.tsx
@@ -17,18 +17,21 @@
 
 const poem = {
   lines: [
-      'I write, erase, rewrite',
-      'Erase again, and then',
-      'A poppy blooms.',
-  ],
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.',
+  ]
 };
 
 export default function Poem() {
   return (
-      <article>
-          {poem.lines.map((line, index) => (
-              <p key={index}>{line}</p>
-          ))}
-      </article>
+    <article>
+      {poem.lines.map((line, index) => (
+        <div key={index}>
+          <p>{line}</p>
+          {index < poem.lines.length - 1 && <hr />}
+        </div>
+      ))}
+    </article>
   );
 }
