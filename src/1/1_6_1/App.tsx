29d17ab6508edd3111ea13ab4a658ffe59182e07
AlenaMozjerina <alenamozjerina10@gmail.com>
2025-05-12 01:00:04 +0500

    1_6_1

--- src/1/1_6_1/App.tsx
+++ src/1/1_6_1/App.tsx
@@ -4,28 +4,23 @@
 */
 
 function Item({ name, isPacked }: { name: string; isPacked: boolean }) {
-  return (
+    return (
       <li className="item">
-          {name} {isPacked && '✔'}
+        {name} {isPacked ? '✔' : '❌'}
       </li>
-  );
-}
-
-export default function PackingList() {
-  return (
+    );
+  }
+  
+  export default function PackingList() {
+    return (
       <section>
-          <h1>Sally Ride's Packing List</h1>
-          <ul>
-              <Item isPacked={true} name="Space suit" />
-              <Item
-                  isPacked={true}
-                  name="Helmet with a golden leaf"
-              />
-              <Item
-                  isPacked={false}
-                  name="Photo of Tam"
-              />
-          </ul>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+          <Item isPacked={true} name="Space suit" />
+          <Item isPacked={true} name="Helmet with a golden leaf" />
+          <Item isPacked={false} name="Photo of Tam" />
+        </ul>
       </section>
-  );
-}
+    );
+  }
+  
