1fdcd9f4907346a475ac7663be23bd59890d172b
AlenaMozjerina <alenamozjerina10@gmail.com>
2025-05-12 01:51:17 +0500

    1_7_1

--- src/1/1_7_1/App.tsx
+++ src/1/1_7_1/App.tsx
@@ -16,24 +16,34 @@ export type Person = {
   imageId: string;
 }
 
-export default function List() {
-    const listItems = people.map((person) => (
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
+function PersonList({ title, people }: { title: string; people: Person[] }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ul>
+        {people.map(person => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
             <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
+              <b>{person.name}:</b>{' '}
+              {person.profession} known for {person.accomplishment}
             </p>
-        </li>
-    ));
-    return (
-        <article>
-            <h1>Scientists</h1>
-            <ul>{listItems}</ul>
-        </article>
-    );
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function List() {
+  const chemists = people.filter(p => p.profession === 'chemist');
+  const others = people.filter(p => p.profession !== 'chemist');
+
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <PersonList title="Chemists" people={chemists} />
+      <PersonList title="Other scientists" people={others} />
+    </article>
+  );
 }
